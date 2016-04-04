1354a171e00616bd4bff3925b89bcd8ea8f3ba4a
fmsuvM <k222.tkd.fms@gmail.com>
2016-04-04 13:29:17 +0900

    second

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 //slack botokit using by Botkit & JavaScript
 
+//token
+var key = 'xoxb-31307312016-caph7L7wKlvgtuyl87wP3X6e';
 
-var bt = require('botkit');
+var bt = require('botkit');//botkit 使う
+
+//tokenが空の場合
+if(key === ''){
+    console.log('Error: Not token');
+    process.exit(1);
+}
+
 var controller = bt.slackbot({
-    debug: false
+    debug: false, //debugは最初false, 複雑担ってきたらtrue
 });
-controller.spawn({
-    token: 'XXXXXXXXXXXXXXXXX'
-}).startRTM();
 
+controller.spawn({
+    token: key //token設定
+}).startRTM(function(err){
+    if(err){//error処理
+        throw new console.error(err);
+    }
+});//startRTMでbot起動
+
+//event handlerはon()かhear(). どっちかに統一した方がいいかも
+//個人的にはhears. 何か「聴いてる」感が欲しい
 controller.on('bot_channel_join', function(bot, message){
-    bot.reply(message, 'Thank you!');
+    return bot.reply(message, 'Thank you!');
+});
+
+controller.hears('hey you', ['direct_mention'], function(bot,message){
+    return bot.reply(message, 'ｳｪｲｿｲﾔ');
 });
