0799d4af3bb8d9650fa42159fe49389abccccc22
fmsuvM <k222.tkd.fms@gmail.com>
2016-04-09 21:30:58 +0900

    add a ask

--- index.js
+++ index.js
@@ -31,15 +31,42 @@ controller.hears('How are you?', 'direct_mention', function(bot, message) {
     //console.log(message); //出力
     bot.startConversation(message, function(err, convo) {
         console.log('conversation message log');
-        console.log(message); //出力 = 上のmessageと同じ
+        //console.log(message); //出力 = 上のmessageと同じ
         console.log('conversation convo log');
-        console.log(convo); //出力
+        //console.log(convo); //出力
 
         convo.say('Hello!!');
         convo.say('I\'m hungry.');
 
-        console.log('new bot log');
-        //上とは別物っぽい?
+        convo.ask('Are you hungry?', [
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo){
+                    convo.say('Oh...');
+                    convo.next();
+                }
+            },
+            {
+                pattern: bot.utterances.no,
+                callback: function(response, convo){
+                    convo.say('That\'s good.');
+                    convo.next();
+                }
+            },
+            {
+                default: true,
+                callback: function(response, convo){
+                    convo.say('Ok. Have a nice day.');
+                    convo.repeat();
+                    convo.next();
+                }
+            }
+        ]);
+
+        //console.log('new bot log');
+        //use_nameとfallback以外反応してる.
+        //これの公式referenceよもう
+        /*
         var message_with_attachments = {
             'user name': 'tkd_bot',
             'text': 'This message is test.',
@@ -51,21 +78,10 @@ controller.hears('How are you?', 'direct_mention', function(bot, message) {
             }],
             'icon_url': 'http://lorempixel.com/48/48'
         };
+        */
 
-        convo.say(message_with_attachments);
+        //convo.say(message_with_attachments);
 
 
     });
 });
-
-/*
-controller.hears('bot_channel_join', function(bot, message){
-    return bot.reply(message, 'Thank you!');
-});
-
-controller.hears('hey you', ['direct_mention'], function(bot,message){
-    console.log(message);
-    //console.log(bot);
-    return bot.reply(message, 'ｳｪｲｿｲﾔ');
-});
-*/
